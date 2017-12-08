bf650cb3dc348d3784e77419e8a07ffbcd39b574
rmit-s3548038-Zhen-Duan <duan2439@gmail.com>
2017-12-08 23:33:23 +1100

    add shopping-cart function with cart view

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
@@ -10,6 +11,34 @@ router.get('/', function(req, res, next) {
       res.render('shop/index', { title: 'Shopping Cart', products: docs });
 
   });
+});
+
+//Add to cart
+router.get('/add-to-cart/:id', function(req, res, next){
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+    Product.findById(productId, function(err, product){
+        if(err){
+            return res.redirect('/');
+        }
+
+        cart.add(product, product.id);
+        //store the cart in the session
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    });
+});
+
+//Shopping-cart
+router.get('/shopping-cart', function(req, res, next){
+    if(!req.session.cart){
+        return res.render('shop/shopping-cart', {products: null});
+    }
+
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 
